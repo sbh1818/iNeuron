45802f8453ea59dedd42f85bfc00d6dd66ed2e28
sbh1818 <105543824+sbh1818@users.noreply.github.com>
2022-05-14 22:36:09 +0530

    Add files via upload

---
+++ Python Basics Assignments_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+*
+hello
+-87.8
+-
+/
++
+6
+
+ANSWER-
+* : operator
+hello : value
+-87.8 : value
+- : operator
+/ : operator
++ : operator
+6 : value2. What is the difference between string and variable?
+Ans:
+String: Strings are collections of characters, sysmbols, numbers etc
+variable: Variables hold values such as strings, numbers 3. Describe three different data types.
+Ans:
+Different data types in python are,
+integers: whole numbers (5,-34,145 etc)
+floats: fractions (23.34,546.67 etc)
+complex numbers: numbers with real and imaginary parts such as 3+4j
+strings: collections of characters, sysmbols, numbers etc such as 'abcd','xyz123','*ghj$%' etc4. What is an expression made up of? What do all expressions do?
+Ans:
+Expressions are made up of values, operators etc. expressions carry out particular tasks.5. This assignment statements, like spam = 10. What is the difference between an
+expression and a statement?
+Ans: When as expression is evaluated, it always produces a value such as 10 in this example. Whereas it is not always the case that a statement will produce a value.6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+Ans: 237. What should the values of the following two terms be?
+&#39;spam&#39; + &#39;spamspam&#39;
+&#39;spam&#39; * 3
+
+Ans: answer for both is 'spamspamspam'8. Why is eggs a valid variable name while 100 is invalid?
+Ans: Variables are used to hold values. eggs itself don't have any values so it can hold one whereas, 100 is an integer value so it can't be used as variable.9. What three functions can be used to get the integer, floating-point number, or string
+version of a value?
+Ans: int(),float(),str()10. Why does this expression cause an error? How can you fix it?
+&#39;I have eaten &#39; + 99 + &#39; burritos.&#39;
+Ans: String and integer can not be concatenated. A valid expression will be 'I have eaten'+'99'+'burritos'
+# In[ ]:
+
+
+
+
