9053002605f6c4e8645268b61a7eda4f7e82f8fc
sbh1818 <105543824+sbh1818@users.noreply.github.com>
2022-05-18 11:58:33 +0530

    Update Python Basics Assignments_5.py

--- Python Basics Assignments_5.py
+++ Python Basics Assignments_5.py
@@ -18,7 +18,9 @@ dict['foo']=42
 dict
 
 3. What is the most significant distinction between a dictionary and a list?
-Ans: Dictionary contains data in 'key:value' format whereas, lists contain only values.4. What happens if you try to access spam['foo'] if spam is {'bar': 100}?
+Ans: Dictionary contains data in 'key:value' format whereas, lists contain only values.
+    
+4. What happens if you try to access spam['foo'] if spam is {'bar': 100}?
 Ans: It'll throw KeyError
 # In[14]:
 
@@ -29,12 +31,18 @@ spam['foo']
 
 5. If a dictionary is stored in spam, what is the difference between the expressions 'cat'; in spam and
 'cat' in spam.keys()?
-Ans: 'cat stored in the dictionary spam is a variable and can be of any type. Whereas 'cat' in spam.keys() is a dict_keys object.6. If a dictionary is stored in spam, what is the difference between the expressions 'cat'; in spam and
+Ans: 'cat stored in the dictionary spam is a variable and can be of any type. Whereas 'cat' in spam.keys() is a dict_keys object.
+    
+6. If a dictionary is stored in spam, what is the difference between the expressions 'cat'; in spam and
 'cat' in spam.values()?
-Ans: 'cat stored in the dictionary spam is a variable and can be of any type. Whereas 'cat' in spam.values() is a dict_values object.7. What is a shortcut for the following code?
+Ans: 'cat stored in the dictionary spam is a variable and can be of any type. Whereas 'cat' in spam.values() is a dict_values object.
+    
+7. What is a shortcut for the following code?
 if 'color' not in spam:
     spam['color'] = 'black'
-Ans: spam.setdefault('color', 'black')8. How do you 'pretty print' dictionary values using which module and function?
+Ans: spam.setdefault('color', 'black')
+    
+8. How do you 'pretty print' dictionary values using which module and function?
 Ans:pprint.pprint()
 # In[24]:
 
