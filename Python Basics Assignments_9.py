6fd1fa8ace5b2744f2ebdd866949eff1d473a8bd
sbh1818 <105543824+sbh1818@users.noreply.github.com>
2022-05-23 21:16:03 +0530

    Update Python Basics Assignments_9.py

--- Python Basics Assignments_9.py
+++ Python Basics Assignments_9.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 1. To what does a relative path refer?
-Ans: A relative path that depicts the location of a file or folder is relative to the current working directory. Unlike absolute paths, relative paths contain information that is only relative to the current document within the same website, which avoids the need to provide a full absolute path.2. What does an absolute path start with your operating system?
-Ans: Absolute path starts with the drive name like C:,D: etc3. What do the functions os.getcwd() and os.chdir() do?
+Ans: A relative path that depicts the location of a file or folder is relative to the current working directory. Unlike absolute paths, relative paths contain information that is only relative to the current document within the same website, which avoids the need to provide a full absolute path.
+  
+2. What does an absolute path start with your operating system?
+Ans: Absolute path starts with the drive name like C:,D: etc
+      
+3. What do the functions os.getcwd() and os.chdir() do?
 Ans: os.getcwd() returns the current working directory
-os.chdir() can be used to switch directory.4. What are the . and .. folders?
+os.chdir() can be used to switch directory.
+
+4. What are the . and .. folders?
 Ans: . refers to current directory
-.. refers to parent directory5. In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
-Ans: dir name is eggs. base name is bacon6. What are the three “mode” arguments that can be passed to the open() function?
-Ans: read mode ('r'), write mode ('w') and read and write mode ('r+')7. What happens if an existing file is opened in write mode?
-Ans: The content of the file will be erased and overwritten.8. How do you tell the difference between read() and readlines()?
+.. refers to parent directory
+
+5. In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+Ans: dir name is eggs. base name is bacon
+
+6. What are the three “mode” arguments that can be passed to the open() function?
+Ans: read mode ('r'), write mode ('w') and read and write mode ('r+')
+  
+7. What happens if an existing file is opened in write mode?
+Ans: The content of the file will be erased and overwritten.
+  
+8. How do you tell the difference between read() and readlines()?
 Ans: read() reads the whole file as one single string. readlines() reads one line at a time.9. What data structure does a shelf value resemble?
 Ans: dictionary.
